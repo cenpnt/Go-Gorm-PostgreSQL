e7548c2c776811bd27317cbc67bedc8a8acfb56c
Pannatat Sribusarakham <cenpolarbears@gmail.com>
2024-12-12 23:30:31 +0700

    create user table with a one to many relationship with post

--- models/postModel.go
+++ models/postModel.go
@@ -2,8 +2,16 @@ package models
 
 import "gorm.io/gorm"
 
+type User struct {
+	gorm.Model
+	Email    string `json:"email" gorm:"unique;not null"`
+	Password string `json:"password" gorm:"not null"`
+	Posts    []Post `json:"posts"`
+}
+
 type Post struct {
 	gorm.Model
-	Title string
-	Body string
+	Title  string `json:"title" gorm:"not null"`
+	Body   string `json:"body"`
+	UserID uint   `json:"user_id"`
 }
